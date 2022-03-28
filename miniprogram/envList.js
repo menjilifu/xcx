3b7741a4604f54522c1894e8cccd287626d37ac4
menjilifu <27811829@qq.com>
2022-03-29 00:27:52 +0800

    First Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-8gevadupf08e735a","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-8gevadupf08e735a --n quickstartFunctions --r --project "C:\Users\Administrator\WeChatProjects\miniprogram-2" --report_first --report
